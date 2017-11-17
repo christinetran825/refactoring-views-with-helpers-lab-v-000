68b38a414653f751c5aadefd6188e2bad37120a0
Christine Tran <christinetran825@gmail.com>
2017-11-17 06:54:22 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,5 +1,9 @@
 module ArtistsHelper
   def display_artist(artist)
-    artist
+    if artist != nil
+      link_to artist_path(artist)
+    else
+      link_to artist_path, "Add Artist"
+    end
   end
 end
